9f81099da5fa639cd5eb56661917faf27b3ed501
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-23 20:55:59 -0500

    all axios calls in actions

--- medcab-app/src/actions/index.js
+++ medcab-app/src/actions/index.js
@@ -1,16 +1,18 @@
-//ACTIONS
+//ACTIONS 
 
-import axios from 'axios';
 
-export const FETCH_USER = 'FETCH_USER'; // .get request to find the user
-export const ADD_USER = 'ADD_USER'; // .post request to add a user
+import React, {useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+export const FETCH_USER = 'FETCH_USER'; // .get request to find the user (added)
+export const ADD_USER = 'ADD_USER'; // .post request to add a user (added)
 export const UPDATE_USER = 'UPDATE_USER'; // .put to update the user
 export const DELETE_USER = 'DELETE_USER'; // .delete the user
 
 //Find the User Dashboard
 export const getUser= () => dispatch => {
   //dispatch({ type: FETCH_USER });
-  axios
+  axiosWithAuth()
   .get(`/users/${user}`)
   .then(res => {
     console.log(res.data);
@@ -24,10 +26,11 @@ export const getUser= () => dispatch => {
 //ADD USER
 export const addUser = (user) => dispatch => {
   dispatch({ type: ADD_USER });
-  axios
+  axiosWithAuth()
   .post(`/users/${user}`, {
     name: user.name,
     age: user.age,
+    //fix this later to actual user form initial values
   })
   .then((res) => {
     //setUser(res.data);
@@ -36,4 +39,36 @@ export const addUser = (user) => dispatch => {
   }) //end then
  .catch(err =>
   console.log('There was an error adding the user', err.message));
+};
+
+//UPDATE User
+
+const updateUser = (props) => {
+  const [ updateUser, setUpdateUser ] =  useState(initialValues)
+useEffect(() => {
+  axiosWithAuth()
+  .put(`/users/${user.id}`)
+  .then((res) => {
+    console.log(res);
+    setUpdateUser(res.data) //or ser it to update the setUser state ????
+  })
+  .catch(err => {
+    console.log('There was an error updating the user', err)
+  })
+}, [id])
+};
+
+//DELETE User
+
+const deleteUser = user => {
+  // make a delete request to delete this user
+  axiosWithAuth()
+    .delete(`/users/${user.id}`, user) //user or setUser or setEditUser ???
+    .then(res => {
+      console.log(res)
+      updateColors(colors.filter( color => {
+        return color.id !== res.data
+      }))
+    })
+    .catch(err => console.log(err.response.message))
 };
