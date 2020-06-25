ad60752f3c610641b153f7ecb4a7af80d3531986
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-24 23:04:41 -0500

    set up is done need to design dashboard and form

--- medcab-app/src/components/UserCard.js
+++ medcab-app/src/components/UserCard.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
-//import { connect } from 'react-redux';
-//import { updateUser, deleteUser } from '../actions';
+import { connect } from 'react-redux';
+import { updateUser, deleteUser, getStrain, deleteStrain } from '../actions';
 
 const UserCard = (props) => {
 useEffect((props) =>{
   props.getUser()
 }, []
 )
+
+useEffect((props) => {
+  props.getStrain()
+})
+
 console.log(props.user)
   // const handleFetch = e => {
   //   e.preventDefault();
@@ -16,11 +21,25 @@ console.log(props.user)
   return (
     <div>
       <div>
-        {props.users.map(user => {
+        {props.user.map(user => {
           return (
             <div key={user.id}>
-              <h2> {user.name} </h2>
-           <p> other info I haven't decided on yet</p>
+              <h2> Welcome {user.name} </h2>
+          
+            <button> Questions </button>
+            <p> reccommendations would go here (map over form results ?)</p>
+            {props.results.map(strain => {
+              return (
+                <div key={strain.id}>
+               <h3>{strain.strain}</h3> 
+               <p>{strain.flavors}</p> 
+              <p>{strain.effects}</p> 
+              <p>{strain.medical}</p> 
+              <p>{strain.type}</p> 
+              <p>{strain.rating}</p> 
+              </div>
+              )
+            })}
             </div>
           )
         })}
@@ -31,15 +50,16 @@ console.log(props.user)
     </div>
   )
 };
-// const mSTP = state => {
-//   return {
-//     users: state.users,
-//     isFetchingData: state.isFetchingData
-//   }
-// }
+const mSTP = state => {
+  return {
+    user: state.user,
+       strain: state.strain,
+    isFetchingData: state.isFetchingData
+  }
+}
 
-// export default connect(mSTP, {updateUser, deleteUser})(UserCard);
+export default connect(mSTP, {updateUser, deleteUser, getStrain, deleteStrain})(UserCard);
 
-export default UserCard;
+
 
 
